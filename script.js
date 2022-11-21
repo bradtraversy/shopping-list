6f8bfeb6878e5a5bf8ef988fc528d213e1213418
Brad Traversy <traversymedia@gmail.com>
2022-11-21 16:47:35 -0500

    Remove & clear items

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 function addItem(e) {
   e.preventDefault();
@@ -22,7 +23,7 @@ function addItem(e) {
 
   itemList.appendChild(li);
 
-  newItem = '';
+  itemInput.value = '';
 }
 
 function createButton(classes) {
@@ -39,5 +40,19 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
