ecbe6bf13eaf9809f85bfca7383036e9bf5e0d9f
Brad Traversy <traversymedia@gmail.com>
2022-11-22 12:25:23 -0500

    Prevent duplicate items

--- script.js
+++ script.js
@@ -31,6 +31,11 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove('edit-mode');
     itemToEdit.remove();
     isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert('That item already exists!');
+      return;
+    }
   }
 
   // Create item DOM element
@@ -98,6 +103,11 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
