23930b00043c626ba68d1bf764e5706dab13bba8
Prem Learner <caprem3501@gmail.com>
2023-10-25 13:31:16 +0530

    added list

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-let output;
+//document.getElementById();
+console.log(document.getElementById('app-title').className);
+console.log(document.getElementById('app-title').getAttribute('class'));
 
-output = document.head;
-output = document.body.children;
-output = document.URL;
-output = document.characterSet;
-//document.forms[0].id = 'new-id';
-output = document.forms[0].id;
-output = document.links;
-output = document.images;
-console.log(output);
+//set attributes
+document.getElementById('app-title').id = 'new-id';
+document.getElementById('new-id').title = 'new-id';
+document.getElementById('new-id').setAttribute('class', 'hi');
+console.log(document.getElementById('new-id'));
+const title = document.getElementById('new-id');
+
+console.log(title.innerText);
+title.textContent = 'hi';
+title.innerHTML = '<strong>Shopping List</strong>';
+title.style.color = 'red';
+
+console.log(document.querySelector('h1'));
+console.log(document.querySelector('#new-id'));
+console.log(document.querySelector('input[type="text"]'));
+console.log(document.querySelector('li:nth-child(2)').innerText);
+const secondItem = document.querySelector('li:nth-child(2)').innerText = 'Apple Juice';
+const secondtem = document.querySelector('li:nth-child(2)');
+secondtem.style.color = 'red';
+const firstItem = document.querySelector('ul').querySelector('li:nth-child(3)');
+console.log(firstItem.innerText);
