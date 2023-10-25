246e28d67bb6d37b6cf137d891a99a5a42fd1c4d
Prem Learner <caprem3501@gmail.com>
2023-10-25 17:08:30 +0530

    create clean and dirty

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-//document.getElementById();
-console.log(document.getElementById('app-title').className);
-console.log(document.getElementById('app-title').getAttribute('class'));
+function createListItem(item) {
+  const li = document.createElement('li');
+  li.innerHTML = `${item}
+  <button class="remove-item btn-link text-red">
+    <i class="fa-solid fa-xmark"></i>
+  </button>`
+  document.querySelector('.items').appendChild(li);
+}
+function createNewItem(item) {
+  const li = document.createElement('li');
+  const button = document.createElement('button');
+  button.className = "remove-item btn-link text-red";
+  const i = document.createElement('i');
+  i.className = "fa-solid fa-xmark";
+  button.appendChild(i)
+  li.appendChild(document.createTextNode(item))
+  li.appendChild(button);
 
-//set attributes
-document.getElementById('app-title').id = 'new-id';
-document.getElementById('new-id').title = 'new-id';
-document.getElementById('new-id').setAttribute('class', 'hi');
-console.log(document.getElementById('new-id'));
-const title = document.getElementById('new-id');
-
-console.log(title.innerText);
-title.textContent = 'hi';
-title.innerHTML = '<strong>Shopping List</strong>';
-title.style.color = 'red';
-
-console.log(document.querySelector('h1'));
-console.log(document.querySelector('#new-id'));
-console.log(document.querySelector('input[type="text"]'));
-console.log(document.querySelector('li:nth-child(2)').innerText);
-const secondItem = document.querySelector('li:nth-child(2)').innerText = 'Apple Juice';
-const secondtem = document.querySelector('li:nth-child(2)');
-secondtem.style.color = 'red';
-const firstItem = document.querySelector('ul').querySelector('li:nth-child(3)');
-console.log(firstItem.innerText);
+  document.querySelector('.items').appendChild(li);
+}
+function createButton(classes) {
+  
+}
+ createListItem('eggs');
+createNewItem('Cheese');
