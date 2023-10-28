1b849b1089205986eba2d3f1d2e4672919f03390
Prem Learner <caprem3501@gmail.com>
2023-10-28 23:19:02 +0530

    Mouse Events

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-const clearBtn = document.querySelector('#clear');
+const logo = document.querySelector('img');
 
-// clearBtn.onclick = function () {
-//   alert('Clear Items');
-// }
-function onClear() {
-  const items = document.querySelector('ul');
-  const itemList = document.querySelectorAll('li');
- // items.innerHTML = '';
-
-  while (items.firstChild) {
-    items.removeChild(items.firstChild);
+const onClick = () => console.log('click event');
+const onRightClick = () => console.log('right click event');
+const onMouseDown = () => console.log('mouse down event');
+const onMouseup = () => console.log('mouse up event');
+const onMouseWheel = () => console.log('mouse wheel event');
+const onMouseover = () => console.log('mouse over event');
+const onMouseout = () => console.log('mouse out event');
+const onDragStart = () => console.log('Drag start event');
+const onDrag = () => console.log('Drag event');
+const onDragEnd = () => console.log('Drag end event');
+const onDoubleClick = () => {
+  if (document.body.style.backgroundColor != 'purple') {
+    document.body.style.backgroundColor = 'purple';
+  }
+  else {
+    document.body.style.backgroundColor = 'white';
   }
 }
-
-//addEventListener()
-//clearBtn.addEventListener('click', () =>alert('clear items'));
-clearBtn.addEventListener('click', onClear);
-
-// setTimeout(()=>clearBtn.removeEventListener('click',onClear()),5000)
-
-// setTimeout(() => clearBtn.click(), 5000);
+//Event Listeners
+logo.addEventListener('click', onClick);
+logo.addEventListener('dblclick', onDoubleClick);
+logo.addEventListener('contextmenu', onRightClick);
+logo.addEventListener('mousedown', onMouseDown);
+logo.addEventListener('mouseup', onMouseup);
+logo.addEventListener('wheel', onMouseWheel);
+logo.addEventListener('mouseover', onMouseover);
+logo.addEventListener('mouseout', onMouseout);
+logo.addEventListener('dragstart', onDragStart);
+logo.addEventListener('drag', onDrag);
+logo.addEventListener('dragend', onDragEnd);
